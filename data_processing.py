bc7f55f9d81b564051702f0ae7dd68f9eb817bc5
Pasin <pasin0931@gmail.com>
2025-11-05 11:59:44 +0700

    Commit to work on pc

--- data_processing.py
+++ data_processing.py
@@ -9,9 +9,19 @@ with open(os.path.join(__location__, 'Cities.csv')) as f:
     for r in rows:
         cities.append(dict(r))
 
-# print(cities)
-# print()
-# print()
+def filter(condition, dict_):
+    temp = []
+    for i in dict_:
+        if condition(i):
+            temp.append(i)
+    return temp
+    
+def aggregate(key_, func_, dict_):
+    temp = []
+    for i in dict_:
+        for j in i:
+            print(j)
+        
 
 # Print first 5 cities only
 for city in cities[:5]:
@@ -33,47 +43,27 @@ print()
 
 # Print all cities in Germany
 print("Cities in Germany")
-for i in cities:
-    if i["country"].lower() == "germany":
-        print(f"- {i["city"]}")
+filtered_list = filter(lambda x: x["country"] == "Germany", cities)
+aggregate("country", lambda x: x, filtered_list)
 print()
 
 # Print all cities in Spain with a temperature above 12°C
 print("Cities in Spain with a temperature above 12°C")
-for i in cities:
-    if i["country"].lower() == "spain" and float(i["temperature"]) > 12:
-        print(f"- {i["city"]}")
+
 print()
 
 # Count the number of unique countries
 print("Unique countries")
-coun_ = []
-for i in cities:
-    if i["country"] not in coun_:
-        coun_.append(i["country"])
-    else:
-        continue
-print(len(coun_))
+
 print()
 
 
 # Print the average temperature for all the cities in Germany
 print("Average temperature for all the cities in Germany")
-count = 0
-sum_ = 0
-for i in cities:
-    if i["country"].lower() == "germany":
-        count += 1
-        sum_ += float(i["temperature"])
-avg_ = sum_ / count
-print(avg_)
+
 print()
 
 # Print the max temperature for all the cities in Italy
 print("Max temperature for all the cities in Italy")
-temp_ = []
-for i in cities:
-    if i["country"].lower() == "italy":
-        temp_.append(float(i["temperature"]))
-print(max(temp_))
+
 print()
